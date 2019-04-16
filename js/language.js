17403886d6785b17b19369364eef2e2b3b56921d
Eugene Kolomiets <kolomiets1990@gmail.com>
2019-04-16 08:16:21 +0300

    languages

--- js/language.js
+++ js/language.js
@@ -1,21 +1,61 @@
-var arrLang = {
+const languages = {
   en: {
-    aboutUs: "About Us",
-    service: "Our Services",
-    scopes: "Scopes"
+    chooseLang: "Choose language:",
+    //Navigation
+    menuAboutUs: "About us",
+    menuService: "Services",
+    menuScopes: "Directions",
+    menuSteps: "Steps",
+    menuContacts: "Contacts"
+    //About Us Section
   },
-  ua: {
-    aboutUs: "Про нас",
-    service: "Послуги",
-    scopes: "Напрямки"
+  uk: {
+    chooseLang: "Виберіть мову:",
+    //Navigation
+    menuAboutUs: "Про нас",
+    menuService: "Послуги",
+    menuScopes: "Напрямки",
+    menuSteps: "Кроки",
+    menuContacts: "Контакти"
+    //About Us Section
+  },
+  de: {
+    chooseLang: "Sprache wählen:",
+    //Navigation
+    menuAboutUs: "Über uns",
+    menuService: "Dienstleistungen",
+    menuScopes: "Richtungen",
+    menuSteps: "Schritte",
+    menuContacts: "Kontakte"
+    //About Us Section
   }
 };
-document.onload = function() {
-  console.log("hello");
-  $(".translate").click(function() {
-    var lang = $(this).attr("id");
-    $(".lang").each(function(index, element) {
-      $(this).text(arrLang[lang][$(this).attr("key")]);
+
+const browserLanguage = navigator.userLanguage || navigator.language;
+const elementsForTranslate = document.querySelectorAll("[text-translate]");
+
+const changeLang = language => {
+  for (const element of elementsForTranslate) {
+    element.innerHTML =
+      languages[language][element.getAttribute("text-translate")];
+  }
+};
+
+window.onload = () => {
+  const firstLetters = browserLanguage.substring(0, 2);
+  if (firstLetters === "en" || firstLetters === "uk" || firstLetters === "de") {
+    changeLang(firstLetters);
+    return;
+  }
+  changeLang("en");
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  const langChangeBtn = document.querySelectorAll(".translate");
+  langChangeBtn.forEach(item => {
+    item.addEventListener("click", function(e) {
+      const langId = this.getAttribute("id");
+      changeLang(langId);
     });
   });
-};
+});
